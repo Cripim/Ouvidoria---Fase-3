a94c1399d07d0397a29eb5a5ca049790cc89184b
Diego Tavares <102764794+Cripim@users.noreply.github.com>
2022-12-06 16:26:09 -0300

    :zap: Melhoria do Menu
    Removendo o "int" do input do menu para evitar erros.

--- main.py
+++ main.py
@@ -22,25 +22,24 @@ while rodando:
   print("[5] Excluir Itens")
   print("[6] Sair do Sistema")
 
-  ouvidoriaFacisa = int(
-    input(
+  ouvidoriaFacisa = input(
       "Digite 1 para inserir algum elogio, 2 para registrar alguma reclamação, 3 para sugerir ideias, 4 para listar itens, 5 para exluir itens ou 6 para sair do sistema: "
-    ))
+    )
 
-  if (ouvidoriaFacisa == 1):
+  if (ouvidoriaFacisa == '1'):
       elogio = input("Insira seu elogio aqui:")
       m.inserir('elogio', elogio)
       print()
       print('Elogio registrado!')  
       print()
-  elif(ouvidoriaFacisa == 2):
+  elif(ouvidoriaFacisa == '2'):
     reclamacao = input("Insira sua reclamação aqui:")
     m.inserir('reclamação', reclamacao)
     print()
     print('Reclamção registrada!')
     print()
     
-  elif(ouvidoriaFacisa == 3):
+  elif(ouvidoriaFacisa == '3'):
     ideias = input("Insira sua sugestão/ideia aqui:")
     m.inserir('ideias', ideias)
     print()
@@ -49,7 +48,7 @@ while rodando:
     
 
   
-  elif(ouvidoriaFacisa == 4):
+  elif(ouvidoriaFacisa == '4'):
       print('Para listar elogios (1), para listar reclamação (2), para listar ideias(3)')
       listar = input('Digite uma opção: ')
 
@@ -67,7 +66,7 @@ while rodando:
         print()
 
    
-  elif (ouvidoriaFacisa == 5):
+  elif (ouvidoriaFacisa == '5'):
         print('')
         print('1 - Apagar comentário especifico')
         print('2 - Apagar tudo')
@@ -99,6 +98,6 @@ while rodando:
                 parametro = int(input('digite o indice que deseja apagar da lista de sugestão:'))
                 m.apagar_especifico(apagar2, parametro)
                 print(f"Itens restantes: {m.listaIdeias}")
-  elif(ouvidoriaFacisa == 5):
+  elif(ouvidoriaFacisa == '5'):
    rodando = False
    print("Você saiu do sistema! Volte sempre!")
